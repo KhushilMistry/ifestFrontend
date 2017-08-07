c68eec3508f27d12922d68c320bd03d4e9a2afde
Khushil Mistry <khushil324@gmail.com>
2017-08-07 10:21:18 +0530

    Initial Commit with index page

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".button-class").click(function () {
+
+
+    var effect = 'slide';
+    var options = { direction: 'right' };
+    var duration = 500;
+
+    $('.top-navbar-links').toggle(effect, options, duration);
+
+    $("#top-navbar-content").toggle();
+    $("#top-navbar-cross").toggle();
+  });
+});
